a89dd3e08c1303fb66b9e2fdb8db5e9a6c12ebfa
Kaiwen Sun <skw_kevin@126.com>
2018-10-07 18:20:21 -0700

    remove flask-babel

--- app/shared/utils.py
+++ app/shared/utils.py
@@ -1,5 +1,9 @@
 import datetime
+
 from flask import url_for
+from flask import request
+
+from app import translations
 
 def timestamp(fmt="%Y%m%d"):
     return datetime.datetime.utcnow().strftime(fmt)
@@ -12,3 +16,43 @@ def static_url_for(filename):
     return url_for('static',
                     filename=filename,
                     statics_version=_statics_version())
+
+def gettext(string, *args):
+    return LazyString(_translator, string, *args)
+
+def _translator(string, *args):
+    locale = _get_locale()
+    dictionary = translations.get_dicts().get(locale, {})
+    translated_string = dictionary.get(string, string)
+    if args:
+        return dictionary.get(translated_string % args)
+    else:
+        return translated_string
+
+def _get_locale():
+    if request:
+        loc = request.accept_languages.best_match(['zh_CN', 'zh_TW'])
+        loc = loc or request.accept_languages.best_match(['zh'])
+        loc = loc or request.accept_languages.best_match(['en'])
+        return loc
+    else:
+        return None
+
+class LazyString(str):
+    def __new__(cls, value, *args, **kwargs):
+        return super(LazyString, cls).__new__(cls, value)
+
+    def __init__(self, func, string, *args):
+        self.func = func
+        self.string = string
+        self.args = args
+
+    def __repr__(self):
+        if len(self.string) <= 30:
+            string = self.string[:30]
+        else:
+            string = "{}...".format(self.string[:27])
+        return "<{}, {}>".format(self.__class__.__name__, string)
+
+    def __str__(self):
+        return self.func(self.string, *self.args)
--- scripts/bash_utils.sh
+++ scripts/bash_utils.sh
@@ -10,22 +10,3 @@ activate_venv() {
         source "${LINUX_VENV_PATH}"
     fi
 }
-
-build_translations() {
-    if [ -f "app/translations/messages.pot" ]; then
-        echo "A translations messages.pot file is found. Please make sure messages.mo and messages.po files are in good state."
-    else
-        declare -a locales=("zh_Hans_CN" "zh_Hant_TW")
-        pybabel extract -F config/babel.cfg -k lazy_gettext -o app/translations/messages.pot --input-dirs=.
-        for locale in "${locales[@]}"
-        do
-            pybabel init -i app/translations/messages.pot -d app/translations -l ${locale}
-        done
-        echo "Don't forget to edit these messages.po file(s) and run 'paybabel compile'"
-        for locale in "${locales[@]}"
-        do
-            echo "   - app/translations/${locales[@]}/LC_MESSAGES/messages.po"
-        done
-    fi
-    pybabel compile -d app/translations
-}
