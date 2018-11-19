d79fb2d60481baac0020b1e38522a382d1b8aa01
Kaiwen Sun <skw_kevin@126.com>
2018-11-19 01:47:18 -0800

    friendly alert messages

--- app/static/js/utils.js
+++ app/static/js/utils.js
@@ -1,6 +1,53 @@
-window.chessPy = {
-    'alert': function(message) {
-        var message = message || "Oops! Something went wrong!";
-        bootbox.alert(message);
+(function() {
+    'strict'
+
+    window.chessPy = {
+        'alert': function(message, alertType, dismissTime, classes) {
+            if (++(window.chessPy._alertCnt) > window.chessPy._alertLimit) {
+                window.chessPy._alertQueue.push(arguments);
+            } else {
+                window.chessPy._showAlert.apply(this, arguments);
+            }
+        },
+        '_showAlert': function(message, alertType, dismissTime, classes) {
+            message = message || '{{ utils.gettext("Oops! Something went wrong!") }}';
+            alertType = alertType || 'info';
+            if (dismissTime === undefined) {
+                dismissTime = 5000;
+            }
+            var $alert = $('<div/>').
+                addClass('alert alert-' + alertType).
+                addClass(classes).
+                attr('role', 'alert').
+                html(message).
+                css('display', 'none').
+                prependTo('#messages-panel')
+                .animate({
+                    opacity: 'show',
+                    height: 'show',
+                    padding: 'show'
+                },
+                400,
+                'swing',
+                setTimeout(function() {
+                        if (dismissTime !== 0) {
+                            window.chessPy.removeAlert($alert);
+                        }
+                }, dismissTime)
+            );
+        },
+        'removeAlert': function($alert) {
+            $alert.fadeOut(function() {
+                $alert.remove();
+                --(window.chessPy._alertCnt);
+                args = window.chessPy._alertQueue.shift();
+                if (args !== undefined) {
+                    window.chessPy._showAlert.apply(this, args);
+                }
+            });
+        },
+        '_alertLimit': 3,
+        '_alertCnt': 0,
+        '_alertQueue': []
     }
-}
+})();
